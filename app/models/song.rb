2caebe60bfa082b1b15004e9b10bc97cb201230b
Daniel Sasser <danielbsasser@gmail.com>
2020-04-26 18:35:32 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: true
-  validates :released, inclusion: {in %w(true false)}
+  validates :released, inclusion: {in: %w(true false)}
   validates :artist_name, presence: true
 end
