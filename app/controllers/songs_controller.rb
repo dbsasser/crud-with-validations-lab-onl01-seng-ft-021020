07bd2eed894d7d51d4ac488d9db2174a091fa40d
Daniel Sasser <danielbsasser@gmail.com>
2020-04-26 18:53:05 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -36,6 +36,6 @@ class SongsController < ApplicationController
   private 
 
   def song_params
-    params.require(:songs).permit(:title, :released, :release_year, :artist_name, :genre)
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
   end
 end 
