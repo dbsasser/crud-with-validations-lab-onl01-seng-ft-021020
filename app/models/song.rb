79716faaebd11a1d4cee5a30955c50c60b7a7b1e
Daniel Sasser <danielbsasser@gmail.com>
2020-04-26 19:05:26 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,6 @@ class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   # validates :released, inclusion: {in: %w(true false)}
   validates :artist_name, presence: true
-  validate :release_year_if_released
-
-  def release_year_if_released
-    validates :release_year, presence: true, if: self.released
-  end
+  validates :release_year, presence: true, if: :released
 
 end
