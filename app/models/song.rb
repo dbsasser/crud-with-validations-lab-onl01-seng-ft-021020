c63a29e766cb65f7d31436c64a1cfec945ccc13b
Daniel Sasser <danielbsasser@gmail.com>
2020-04-26 19:10:10 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,5 +3,12 @@ class Song < ApplicationRecord
   # validates :released, inclusion: {in: %w(true false)}
   validates :artist_name, presence: true
   validates :release_year, presence: true, if: :released
+  validate :release_year_cannot_be_in_the_future
+
+  def release_year_cannot_be_in_the_future
+    if release_year.present? && release_year > Date.today.year 
+      errors.add(:release_year, "Can't be in the future")
+    end
+  end
 
 end
